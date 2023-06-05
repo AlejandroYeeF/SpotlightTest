602833f94bad3687cc5cb2feceb0276756396436
Alejandro Yee <alejandro.yee@digitalfemsa.com>
2023-06-05 12:42:39 -0600

    updated project

--- SuperbackCiamOTPTest/Usings.cs
+++
@@ -1,7 +0,0 @@
-// ---------------------------------------------------------------------------------------------
-// <copyright file="Usings.cs" company="DigitalFEMSA">
-// Copyright (c) DigitalFEMSA. All rights reserved.
-// </copyright>
-// ---------------------------------------------------------------------------------------------
-
-global using Xunit;

dd5c319723000becc17c05f0c98cf6eaf95aca0d
yee.alex7@gmail.com <yee.alex7@gmail.com>
2023-06-05 13:01:27 -0600

    Modified 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwMzk1MA/spectral.js?branch_slug=main&token=39461fe0-7c29-4dd5-b448-34afed0d385d";
+export default { extends: ruleset };
